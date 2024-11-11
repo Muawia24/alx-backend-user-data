27cad1094db03c42a626d4d0fec135437591388e
Ahmed Muawia <amuawia666@gmail.com>
2024-11-11 15:10:55 +0200

    5. Request validation!

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,6 +12,31 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+if os.getenv("AUTH_TYPE"):
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+
+@app.before_request
+def auth_handler():
+    """
+    handler before_request
+    """
+    if auth is None:
+        return
+
+    exclude_path = [
+            '/api/v1/status/',
+            '/api/v1/unauthorized/',
+            '/api/v1/forbidden/'
+            ]
+    if auth.require_auth(request.path, exclude_path) is False:
+        return
+    if auth.authorization_header(request) is None:
+        return abort(401)
+    if auth.current_user(request) is None:
+        return abort(403)
 
 
 @app.errorhandler(401)
@@ -20,6 +45,14 @@ def unauthorized(error) -> str:
     Not authorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """
+    Not allowed to access handler
+    """
+    return jsonify({"error": "Forbidden"})
 
 
 @app.errorhandler(404)
---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+API authentication
+"""
+
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        returns True if the path is not in the list of
+        strings excluded_paths.
+        """
+        if path is None or excluded_paths is None:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path not in excluded_paths:
+            return True
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Validates all requests to secure the API
+        """
+        return request.headers.get("Authorization")
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        """
+        return None
