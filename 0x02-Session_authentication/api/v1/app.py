711ab21d8e5cef848bfac2ffac4f475a15a6c2f1
Ahmed Muawia <amuawia666@gmail.com>
2024-11-15 01:32:50 +0200

    5. Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -39,14 +39,19 @@ def auth_handler():
     excluded_paths = [
             '/api/v1/status/',
             '/api/v1/unauthorized/',
-            '/api/v1/forbidden/'
+            '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/'
     ]
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if auth.authorization_header(request) is None:
-        return abort(401)
+
+    cookie = auth.session_cookie(request)
+    if auth.authorization_header(request) is None and cookie is None:
+        abort(401)
+
     if auth.current_user(request) is None:
-        return abort(403)
+        abort(403)
+
     request.current_user = auth.current_user(request)
 
 
