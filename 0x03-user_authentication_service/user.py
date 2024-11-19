b202b795b793e557307a82f3c00a327f8a002cac
Ahmed Muawia <amuawia666@gmail.com>
2024-11-19 13:28:13 +0200

    0. User model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -12,12 +12,24 @@ Base = declarative_base()
 
 class User(Base):
     """
-    User class
+    Attributes:
+        __tablename__ (str): The name of the table in the
+        database where user records are stored.
+        id (int): The unique identifier of the user.
+        email (str): The email address of the user.
+        hashed_password (str): The hashed password of the
+        user.
+        session_id (str): The session ID of the user, used
+        to maintain
+        user sessions.
+        reset_token (str): The reset token of the user,
+        used for password
+        resets.
     """
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
