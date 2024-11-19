c7e79c4ae8ca4eff9e18b9bf9cba533d8c95b40f
Ahmed Muawia <amuawia666@gmail.com>
2024-11-19 14:24:57 +0200

    16. Generate reset password token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -132,9 +132,10 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            reset_token = _generate_uuid()
-            self._db.update(user.id, reset_token=reset_token)
         except NoResultFound:
             raise ValueError("User corresponding to this email is not found")
+
+        reset_token = _generate_uuid()
+        self._db.update(user.id, reset_token=reset_token)
 
         return reset_token
