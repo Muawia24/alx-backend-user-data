e02dc1d712637be0133ee0d6b27a7aa20375bcea
Ahmed Muawia <amuawia666@gmail.com>
2024-11-19 14:57:31 +0200

    18. Update password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -156,10 +156,11 @@ class Auth:
         try:
             user = self._db.find_user_by(reset_token=reset_token)
         except NoResultFound:
-            raise ValueError('User Not Found')
+            raise ValueError('Invalid reset_token')
 
         hashed_pwd = _hash_password(password)
         self._db.update_user(
                 user.id,
                 hashed_password=hashed_pwd,
-                reset_token=None)
+                reset_token=None
+                )
