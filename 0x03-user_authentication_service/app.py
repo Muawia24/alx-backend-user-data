826fdb9a8f1e80daa07b570305fbe8d9ca5a883f
Ahmed Muawia <amuawia666@gmail.com>
2024-11-19 15:44:17 +0200

    19. Update password end-point

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -126,7 +126,7 @@ def update_password() -> str:
     except ValueError:
         abort(403)
 
-    return jsonify({"email": email, "message": "Password updated"})
+    return jsonify({"email": email, "message": "Password updated"}), 200
 
 
 if __name__ == "__main__":
